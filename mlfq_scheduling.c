76b941532fd22fbc274f45154ae93a856fec1054
johnny99a <140450907+johnny99a@users.noreply.github.com>
2024-10-06 21:44:52 -0700

    Update mlfq_scheduling.c

--- mlfq_scheduling.c
+++ mlfq_scheduling.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_QUEUES 3
-#define BOOST_INTERVAL 5 // Set an interval for priority boosting
-#define MAX_COUNT 3 // Maximum count before demotion
+#define NUM_QUEUES 3 // Set number of queues 
+#define INC_AMT 6 // Set an interval for priority boosting
+#define MAX_COUNT 3 // Maximum count before drop
 
 typedef struct _process {
     int pid;
     char name;
-    int size;
-    int priority;
+    int size; // Size of process
+    int priority; // Priority level
     int count; // Counter for the number of time slices used
     struct _process *next;
 } Process_t;
@@ -21,35 +21,35 @@ typedef struct _queue {
 
 int pidSequence = 0;
 int timeSlices[NUM_QUEUES] = {2, 4, 6}; // Time slices for each priority level
-int boost_counter = 0; // Counter for priority boosting
+int inc_counter = 0; // Counter for priority increase
 
 Process_t* createProcess(char NewName, int newSize) {
-    Process_t *result = (Process_t *)malloc(sizeof(Process_t));
-    result->name = NewName;
-    result->next = NULL;
-    result->pid = ++pidSequence;
-    result->size = newSize;
-    result->priority = 0;
-    result->count = 0;
+    Process_t *result = (Process_t *)malloc(sizeof(Process_t)); // Allocates memory in the heap for each process
+    result->name = NewName; // Stores member data into heap
+    result->next = NULL; // ^
+    result->pid = ++pidSequence; // ^
+    result->size = newSize; // ^
+    result->priority = 0; // Initilizes priority level at highest level -- Rule 3
+    result->count = 0; // Intitializes the count
     return result;
 }
 
 Queue_t* createQueue() {
-    Queue_t* result = (Queue_t *) malloc(sizeof(Queue_t));
-    result->first = NULL;
-    result->last = NULL;
+    Queue_t* result = (Queue_t *) malloc(sizeof(Queue_t)); // Allocates memory in the heap
+    result->first = NULL; // Assigns first and last as null as there is no elements yet
+    result->last = NULL; // ^
     return result;
 }
 
 int addToQueue(Queue_t *q, Process_t *p) {
-    if (q == NULL) return -1;
+    if (q == NULL) return -1; 
     if (q->first == NULL) {
-        q->first = p;
-        q->last = p;
-        return 0;
+        q->first = p; // Accesses heap memory of q and member first and assigns it the process
+        q->last = p; // Assigns the process as last because initially it would be the only process
+        return 0; 
     }
-    q->last->next = p;
-    q->last = p;
+    q->last->next = p; // Moves the pointer to the next node and links last process with next
+    q->last = p; // Assigns and ensures last element added is last
     return 0;
 }
 
@@ -57,56 +57,56 @@ Process_t* getRemoveFromQueue(Queue_t *q) {
     if (q == NULL) return NULL;
     if (q->first == NULL) return NULL;
 
-    Process_t* result = q->first;
-    if (q->first == q->last) {
-        q->first = NULL;
-        q->last = NULL;
+    Process_t* result = q->first; // Assigns first element in queue to pointer result
+    if (q->first == q->last) { // Checks if only 1 element in queue and if so assign both members to null
+        q->first = NULL; // ^
+        q->last = NULL; // ^
     } else {
-        q->first = q->first->next;
+        q->first = q->first->next; // If more than 1 element, it moves the pointer to the next element
     }
-    result->next = NULL;
+    result->next = NULL; // Assigns next pointer to null
     return result;
 }
 
-void boostPriorities(Queue_t* queues[]) {
+void increasePrior(Queue_t* queues[]) { // Rule 5
     for (int i = 1; i < NUM_QUEUES; i++) {
         while (queues[i]->first != NULL) {
-            Process_t* p = getRemoveFromQueue(queues[i]);
-            p->priority = 0;
-            p->count = 0; // Reset the counter during boosting
-            addToQueue(queues[0], p);
+            Process_t* p = getRemoveFromQueue(queues[i]); // Removes element from current queue and assigns it to the process p
+            p->priority = 0; // Assigns the member priority in the current process to the highest priority
+            p->count = 0; // Reset the counter when increased
+            addToQueue(queues[0], p); // Moves the process to the highest priority queue
         }
     }
 }
 
 int mlfq(Queue_t* queues[]) {
-    while (1) {
+    while (1) { // Loops until all queues are empty
         int foundProcess = 0;
         for (int i = 0; i < NUM_QUEUES; i++) {
-            if (queues[i]->first != NULL) {
+            if (queues[i]->first != NULL) { // Checks first element of queue is not null
                 foundProcess = 1;
-                Process_t* runningProcess = getRemoveFromQueue(queues[i]);
-                int timeSlice = timeSlices[i];
-                printf(" Process %c with pid = %d from queue %d is running; size start = %d; size left = %d \n",
+                Process_t* runningProcess = getRemoveFromQueue(queues[i]); // Removes first process from queue and assigns it to the running process pointer
+                int timeSlice = timeSlices[i]; // Obtains time slice depending on queue 
+                printf(" Process %c with pid = %d from queue %d is running; size start = %d; size left = %d \n", // Prints current process info
                         runningProcess->name, runningProcess->pid, i, runningProcess->size, runningProcess->size - timeSlice);
 
-                runningProcess->size -= timeSlice;
-                runningProcess->count++;
+                runningProcess->size -= timeSlice; //Subtracts the time slice amount from the size of the process
+                runningProcess->count++; // Increments the count for the process
 
-                if (runningProcess->size > 0) {
-                    if (runningProcess->count >= MAX_COUNT && i < NUM_QUEUES - 1) {
-                        runningProcess->priority++;
-                        runningProcess->count = 0;
+                if (runningProcess->size > 0) { // Checks if process is done
+                    if (runningProcess->count >= MAX_COUNT && i < NUM_QUEUES - 1) { // Checks if max count has been reached for demotion -- Rule 6
+                        runningProcess->priority++; // Increments priority due to priority drop
+                        runningProcess->count = 0; // Resets count
                     }
-                    addToQueue(queues[runningProcess->priority], runningProcess);
-                } else {
+                    addToQueue(queues[runningProcess->priority], runningProcess); // Updates element in correct queue depending on priority level
+                } else { // Process finished
                     printf(" ########### Process %c is finished\n", runningProcess->name);
-                    free(runningProcess);
+                    free(runningProcess); // Frees up the heap memory of running process
                 }
-                boost_counter++;
-                if (boost_counter >= BOOST_INTERVAL) {
-                    boostPriorities(queues);
-                    boost_counter = 0;
+                inc_counter++; // Increments priority counter
+                if (inc_counter >= INC_AMT) { // Rule 5
+                    increasePrior(queues);
+                    inc_counter = 0;
                 }
                 break;
             }
@@ -117,12 +117,12 @@ int mlfq(Queue_t* queues[]) {
 }
 
 int main() {
-    Queue_t* queues[NUM_QUEUES];
+    Queue_t* queues[NUM_QUEUES]; 
     for (int i = 0; i < NUM_QUEUES; i++) {
-        queues[i] = createQueue();
+        queues[i] = createQueue(); // Goes to createQueue function and creates all queues
     }
 
-    addToQueue(queues[0], createProcess('A', 32));
+    addToQueue(queues[0], createProcess('A', 32)); // Creates process first then assigns it to the queue
     addToQueue(queues[0], createProcess('B', 45));
     addToQueue(queues[0], createProcess('C', 15));
     
